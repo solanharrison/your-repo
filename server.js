75a0caed159199fc1fd2c6526f5594e04278766c
mohd saquib <allrounder20240@gmail.com>
2026-02-11 22:06:58 -0800

    Refactor server.js for player respawn logic

--- server.js
+++ server.js
@@ -4,94 +4,55 @@ import { Server } from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
-
-const io = new Server(httpServer, {
-  cors: { origin: "*" }
-});
+const io = new Server(httpServer, { cors: { origin: "*" } });
 
 const PORT = process.env.PORT || 3000;
-
 let players = {};
 
-app.get("/", (req, res) => {
-  res.send("Multiplayer server running.");
-});
-
-// Helper function to reset player position (Respawn)
-function respawnPlayer(id) {
-    if (players[id]) {
-        players[id].x = Math.floor(Math.random() * 700) + 50;
-        players[id].y = Math.floor(Math.random() * 500) + 50;
-        players[id].health = 100;
-    }
-}
-
 io.on("connection", (socket) => {
-  console.log("Player connected:", socket.id);
-
   players[socket.id] = {
-    x: 400,
-    y: 300,
+    x: Math.random() * 1800 + 100,
+    y: Math.random() * 1800 + 100,
     angle: 0,
-    health: 100
+    health: 100,
+    kills: 0
   };
 
   socket.emit("currentPlayers", players);
-  socket.broadcast.emit("newPlayer", {
-    id: socket.id,
-    player: players[socket.id]
-  });
+  socket.broadcast.emit("newPlayer", { id: socket.id, player: players[socket.id] });
 
   socket.on("move", (data) => {
-    if (!players[socket.id]) return;
-
-    players[socket.id].x = data.x;
-    players[socket.id].y = data.y;
-    players[socket.id].angle = data.angle;
-
-    socket.broadcast.emit("playerMoved", {
-      id: socket.id,
-      player: players[socket.id]
-    });
-  });
-
-  socket.on("shoot", (bullet) => {
-    socket.broadcast.emit("bulletFired", {
-      id: socket.id,
-      bullet
-    });
-  });
-
-  // --- NEW: KILL & RESPAWN FEATURE ---
-  socket.on("killPlayer", (victimId) => {
-    if (players[victimId]) {
-      console.log(`${socket.id} killed ${victimId}`);
-      
-      // Tell everyone this player died
-      io.emit("playerKilled", victimId);
-
-      // Give it a small delay before they reappear at a random spot
-      setTimeout(() => {
-        respawnPlayer(victimId);
-        io.emit("newPlayer", {
-          id: victimId,
-          player: players[victimId]
-        });
-      }, 2000); 
+    if (players[socket.id]) {
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
+      players[socket.id].angle = data.angle;
+      socket.broadcast.emit("playerMoved", { id: socket.id, player: players[socket.id] });
     }
   });
 
-  socket.on("pingCheck", () => {
-    socket.emit("pongCheck");
+  socket.on("shoot", (bullet) => {
+    io.emit("bulletFired", { id: socket.id, bullet });
+  });
+
+  socket.on("killPlayer", (victimId) => {
+    if (players[victimId]) {
+        io.emit("playerKilled", victimId);
+        // Respawn Logic
+        setTimeout(() => {
+            if(players[victimId]) {
+                players[victimId].health = 100;
+                players[victimId].x = Math.random() * 1800 + 100;
+                players[victimId].y = Math.random() * 1800 + 100;
+                io.emit("newPlayer", { id: victimId, player: players[victimId] });
+            }
+        }, 3000);
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("Disconnected:", socket.id);
     delete players[socket.id];
     io.emit("playerDisconnected", socket.id);
   });
 });
 
-httpServer.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+httpServer.listen(PORT, () => console.log("Arena Server Live on " + PORT));
